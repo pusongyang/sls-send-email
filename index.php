25d1330853bf12243b022a3be02b75bf5b40bedf
qinyue <songyang.psy@alibaba-inc.com>
2020-05-05 11:29:05 +0800

    feat: 发送邮件服务

--- index.php
+++ index.php
@@ -2,45 +2,45 @@
   include_once 'aliyun-php-sdk-core/Config.php';
   use RingCentral\Psr7\Response;
   use Dm\Request\V20151123 as Dm;
-  //需要设置对应的region名称，如华东1（杭州）设为cn-hangzhou，新加坡Region设为ap-southeast-1，澳洲Region设为ap-southeast-2。
-  $iClientProfile = DefaultProfile::getProfile("cn-shanghai", "your_accessKeyID", "your_accessKeySecret");
-  //新加坡或澳洲region需要设置服务器地址，华东1（杭州）不需要设置。
-  //$iClientProfile::addEndpoint("ap-southeast-1","ap-southeast-1","Dm","dm.ap-southeast-1.aliyuncs.com");
-  //$iClientProfile::addEndpoint("ap-southeast-2","ap-southeast-2","Dm","dm.ap-southeast-2.aliyuncs.com");
-  $client = new DefaultAcsClient($iClientProfile);
-  $request = new Dm\SingleSendMailRequest();
-  //新加坡或澳洲region需要设置SDK的版本，华东1（杭州）不需要设置。
-  //$request->setVersion("2017-06-22");
-  $request->setAccountName("message@mail.jike-serverless.online");
-  $request->setFromAlias("秦粤");
-  $request->setAddressType(1);
-  $request->setTagName("capture");
-  $request->setReplyToAddress("false");
-  $request->setToAddress("ukyo.pu@gmail.com");
-  //可以给多个收件人发送邮件，收件人之间用逗号分开,若调用模板批量发信建议使用BatchSendMailRequest方式
-  //$request->setToAddress("邮箱1,邮箱2");
-  $request->setSubject("邮件主题");
-  $request->setHtmlBody("邮件正文");
-  try {
-      $response = $client->getAcsResponse($request);
-      print_r($response);
-  }
-  catch (ClientException  $e) {
-      print_r($e->getErrorCode());
-      print_r($e->getErrorMessage());
-  }
-  catch (ServerException  $e) {
-      print_r($e->getErrorCode());
-      print_r($e->getErrorMessage());
-  }
-  function handler($request, $context): Response{
+  function handler($req, $context): Response{
+      $fstring = file_get_contents("aliyunConfig.json");
+      $conf = json_decode($fstring, true);
+      $iClientProfile = DefaultProfile::getProfile($conf['region'], $conf['accessKeyId'], $conf['accessKeySecret']);
+      $client = new DefaultAcsClient($iClientProfile);
+      $request = new Dm\SingleSendMailRequest();
+      $email_config = $conf['emailConfig'];
+      $request->setAccountName($email_config['AccountName']);
+      $request->setFromAlias($email_config['FromAlias']);
+      $request->setAddressType(1);
+      $request->setTagName("capture");
+      $request->setReplyToAddress("false");
+
+      $queries = $req->getQueryParams();
+      $request->setToAddress($queries['address']);
+      $request->setSubject($queries['subject']);
+      $request->setHtmlBody($queries['contents']);
+      $response_code = 200;
+      $response_header = array('ServeBy' => ['php', 'faas']);
+      $response_body = 'email send!';
+
+      try {
+          $response = $client->getAcsResponse($request);
+          print_r($response);
+      }
+      catch (ClientException  $e) {
+          $response_body = 'Oops!Client failed to send email!';
+          print_r($e->getErrorCode());
+          print_r($e->getErrorMessage());
+      }
+      catch (ServerException  $e) {
+          $response_body = 'Oops!Server failed to send email!';
+          print_r($e->getErrorCode());
+          print_r($e->getErrorMessage());
+      }
       return new Response(
-          200,
-          array(
-              'custom_header1' => 'v1',
-              'custom_header2' => ['v2', 'v3'],
-          ),
-          'email send!'
+          $response_code,
+          $response_header,
+          $response_body
       );
   }
 ?>
